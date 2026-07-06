ffae76a2e9cc45098367381a7d1970b93c07e799
olium <jeremie.rouxel@outlook.fr>
2026-07-06 16:28:35 +0200

    .

---
+++ making_change/0-making_change.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Determine the fewest number of coins needed to meet a given total."""
+
+
+def makeChange(coins, total):
+    """Return the fewest number of coins needed to meet total.
+
+    Args:
+        coins: list of the values of the coins in your possession.
+        total: the amount to make up.
+
+    Returns:
+        The fewest number of coins needed to meet total, 0 if total is
+        0 or less, or -1 if total cannot be met.
+    """
+    if total <= 0:
+        return 0
+
+    # dp[i] = fewest coins needed to make amount i (float('inf') if unreachable)
+    dp = [0] + [float('inf')] * total
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            if dp[amount - coin] + 1 < dp[amount]:
+                dp[amount] = dp[amount - coin] + 1
+
+    return dp[total] if dp[total] != float('inf') else -1
